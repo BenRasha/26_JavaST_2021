c16a9a1871d6bed88a36b094fc122643808cec74
BenRasha <bben.rasha@gmail.com>
2021-06-24 22:32:05 +0300

    Added resource bundle and internationalization

---
+++ Task04ClassesUML/src/by.training.classesUML/message/MessageManager.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public enum MessageManager {
+    EN(ResourceBundle.getBundle("text", new Locale("en", "EN"))),
+    RU(ResourceBundle.getBundle("text", new Locale("ru", "RU")));
+
+    private ResourceBundle bundle;
+
+    MessageManager(ResourceBundle bundle) {
+        this.bundle = bundle;
+    }
+
+    public void changeResource(Locale locale) {
+        bundle = ResourceBundle.getBundle("text", locale);
+    }
+
+    public String getString(String key) {
+        return bundle.getString(key);
+    }
+
+}
